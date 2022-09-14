f0e65e5f9d62534f584a0d05e3ec428d3b5ed683
bears <bears@bears.zone>
2022-09-13 22:01:54 -0500

    update homepage to list user collections

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,61 +6,50 @@ import { createClient, SchemaFieldTypes, SearchOptions } from "redis";
 import React, { useState, useEffect } from "react";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
-
 import SearchSidebar from "../components/common/SearchSidebar";
+import CollectionView from "../components/common/CollectionView";
+import useSWR from "swr";
 
-// export async function getStaticProps(context) {
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-//   const client = createClient({});
-//   client.connect();
-//   client.on('connect', () => console.log('connected to redis successfully!'));
-//   client.on('error', (err) => console.log('Redis Client Error', err));
-
-//   var output = await client.ft.search('idx:cards', '@type_line:Bear');
-
-//   var foundNames = []
-//   output['documents'].forEach(element => {
-//     foundNames.push(element['value']['name'])
-//   });
-
-//   return {
-//     props: { uri: foundNames }, // will be passed to the page component as props
-//   }
-// }
-
-const Home: React.FC = () => {
-  const [content, setContent] = useState("");
-  const [results, setResults] = useState([]);
-
-  // var out = useSWR('/api/search?q=' + content)
+export default function Home(props) {
+  const [collections, setCollections] = useState([]);
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
+    const data = await fetch("/api/collections/readCollections");
+    const returned = await data.json();
 
-    const api_req = await fetch("/api/search?q=" + content);
-
-    const api_res = await api_req.json();
-
-    setResults(api_res);
+    setCollections(returned);
   };
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="Collection Tracker" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Collection Tracker"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
       </Head>
 
       <Header />
 
       <main className={styles.main}>
+        <button onClick={submitData} />
         <SearchSidebar />
+        <div>
+          {collections.map((element) => {
+            return <p>{element["id"]}</p>;
+          })}
+        </div>
       </main>
 
       <Footer />
     </div>
   );
-};
-
-export default Home;
+}

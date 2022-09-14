be9f543618c8f4002d641337843d8cc777d95527
bears <bears@bears.zone>
2022-09-13 21:59:20 -0500

    create collection view component

---
+++ components/common/CollectionView.tsx
@@ -0,0 +1,10 @@
+import { type } from "os";
+import React, { useState, useEffect } from "react";
+import useSWR from "swr";
+import styles from "../../styles/CollectionViewStyles.module.css";
+
+function CollectionView(props) {
+  return <div className={styles.collectionview}></div>;
+}
+
+export default CollectionView;

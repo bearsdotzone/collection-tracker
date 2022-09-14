2d5556879ea8aea59897a19e2d8b4d0b27d4ee9c
bears <bears@bears.zone>
2022-09-13 21:59:10 -0500

    header tweaks

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,24 +1,24 @@
-import styles from "./HeaderStyles.module.css";
+import styles from "../../styles/HeaderStyles.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => {
   const { data: session } = useSession();
   if (session) {
     return (
-      <>
-        {" "}
-        Signed in as {session.user.email} <br />{" "}
-        <button onClick={() => signOut()}>Sign out</button>{" "}
-      </>
+      <header className={styles.header}>
+        <p>
+          Paper Trail - Signed in as {session.user.email}
+          <button onClick={() => signOut()}>Sign out</button>
+        </p>
+      </header>
     );
   } else {
     return (
-      <>
-        <header className={styles.header}>
-          <p>Paper Trail</p>
-        </header>
-        <button onClick={() => signIn()}>Sign In</button>{" "}
-      </>
+      <header className={styles.header}>
+        <p>
+          Paper Trail <button onClick={() => signIn()}>Sign In</button>
+        </p>
+      </header>
     );
   }
 };
